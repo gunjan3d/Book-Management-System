78f5ab4982b944c11f7720824b5ee2cd1eeea161
gunjan3d <gunjansarode6@gmail.com>
2024-08-19 02:17:55 +0530

    all files

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const MongodbUrl = 'mongodb+srv://gunjan6:Gunjan6@book-store.gzgfm17.mongodb.net/?retryWrites=true&w=majority&appName=book-store';
---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <>
+      <center>
+        <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-600'></div>
+      </center>
+    </>
+
+  )
+}
+
+export default Spinner
